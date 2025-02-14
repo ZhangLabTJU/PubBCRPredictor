1845d415eaf91066008b4079285240e264d70cbe
ZhangLabTJU <89921026+ZhangLabTJU@users.noreply.github.com>
2025-02-14 09:49:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,28 @@ from os import path
 from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
-with open(path.join(here, 'requirements.txt'), 'r', encoding='utf-8') as f:
-    all_reqs = f.read().split('\n')
 
-with open(path.join(here, 'README.md'), 'r', encoding='utf-8') as f:
-    long_description = f.read()
+def read_file(file_path: str) -> str:
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            return file.read()
+    except FileNotFoundError as e:
+        print(f"Error reading file: {e}")
+        return ""
+
+requirements_file = path.join(here, 'requirements.txt')
+long_description_file = path.join(here, 'README.md')
+
+all_reqs = read_file(requirements_file).split('\n')
+long_description = read_file(long_description_file)
 
 install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
 
 setup(
     name='PubBCRPredictor',  
     version='1.0.0',  
-    author='ZJ-lab',  
-    description='PubBCRPredictor,the public antibody prediction module leverages the pre-trained **BCR-V-BERT** model to classi-fy heavy chain antibodies (binary classification) and predict light chain antibodies (regression). This feature allows for the identification of public antibodies using advanced transformer-based sequence analysis.',  
+    author='Qihang Xu, Jian Zhang',  
+    description='PubBCRPredictor, the public antibody prediction module leverages the pre-trained BCR-V-BERT model to classify heavy chain antibodies (binary classification) and predict light chain antibodies (regression).', 
     long_description=long_description, 
     long_description_content_type='text/markdown', 
     author_email='jian_zhang@tju.edu.cn',  
@@ -23,9 +32,8 @@ setup(
     include_package_data=True, 
     packages=find_packages(),  
     install_requires=install_requires, 
-
     classifiers=[
-        'License :: OSI Approved :: CC BY-NC-SA 4.0',
+        'License :: OSI Approved :: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
     ],
